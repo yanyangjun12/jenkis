594b5994f8c2caadf90ba1051f4329b993a4273c
yanyangjun12 <74900245+yanyangjun12@users.noreply.github.com>
2023-04-20 17:47:26 +0800

    Add files via upload

---
+++ java-jenkins/src/main/java/TestMain.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class TestMain {
+
+  @Test
+  public void testInputIsEven(){
+    assertTrue(Main.checkIfInputIsAnEvenNumber(22)); // Assertion
+  }
+}
